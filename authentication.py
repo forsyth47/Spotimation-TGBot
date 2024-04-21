d7c6de6471e1e02d5357f3e9e05eb0632b0a98ab
Reiji <88034696+forsyth47@users.noreply.github.com>
2024-04-21 10:57:01 -0700

    Update authentication.py

--- authentication.py
+++ authentication.py
@@ -2,35 +2,55 @@ import requests
 import json
 import os
 import time
+ 
+#=========VARIABLES=============
+Telegram_BotKey = os.environ['Telegram_BotKey']
+Telegram_ChatId = os.environ['Telegram_ChatId']
 
-playlisturl = "YOUR_PLAYLIST_LINK"
-botkey = os.environ['botkey']
-chatid = 'YOUR_TG_CHAT_ID'
+Spotify_PlaylistUrl = os.environ['Spotify_PlaylistUrl']
+Spotify_ClientId = os.environ['Spotify_ClientId']
+Spotify_ClientSecret = os.environ['Spotify_ClientSecret']
+Spotify_PlaylistId = Spotify_PlaylistUrl.split('playlist/')[1].split('?')[0]
+#=========VARIABLES-END=========
 
-def get_stoken():
-	#auth_code=str(input("Enter code: "))
-	auth_url = 'https://accounts.spotify.com/api/token'
-	payload = {
-	    'client_id': '5f573c9620494bae87890c0f08a60293',
-	    'client_secret': '212476d9b0f3472eaa762d90b19b0ba8',
-	    ##################################
-	#    'grant_type': 'authorization_code',
-	#    'code': auth_code,
-	#    'redirect_uri': 'https://example.com'
-	    ##################################
-	    "grant_type": "client_credentials"
-	}
-	while True:
-		###for authorization_code###
-		#response = requests.post(auth_url, data=payload)
-		###for client_credentials###
-		response = requests.post(auth_url, data=payload, headers={"Content-Type": "application/x-www-form-urlencoded"})
-		access_token = response.json()['access_token']
-		if access_token:
-			with open('keys.json','w') as f:
-				data = {'skey': access_token, 'botkey': botkey, 'chatid': chatid, 'playlisturl': playlisturl}
-				json.dump(data, f, indent=4)
-			break
-		time.sleep(5)
+def Spotify_PlaylistInfo(Spotify_PlaylistId, access_token):
+  endpoint = f"https://api.spotify.com/v1/playlists/{Spotify_PlaylistId}"
+  headers = {"Authorization": f"Bearer {access_token}"}
+
+  try:
+      response = requests.get(endpoint, headers=headers)
+      response.raise_for_status()
+      playlist_data = response.json()
+      return playlist_data['name'], playlist_data['owner']['display_name']
+  except requests.exceptions.RequestException as e:
+      return None, None
+
+
+def getSpotfyAccessToken():
+  auth_url = 'https://accounts.spotify.com/api/token'
+  payload = {
+      'client_id': Spotify_ClientId,
+      'client_secret': Spotify_ClientSecret,
+      ##################################
+      # 'grant_type': 'authorization_code',
+      # 'code': auth_code,
+      # 'redirect_uri': 'https://example.com'
+      ##################################
+      "grant_type": "client_credentials"
+  }
+  while True:
+    #========== for authorization_code ==========#
+    # response = requests.post(auth_url, data=payload)
+    
+    #========== for client_credentials ==========#
+    response = requests.post(auth_url, data=payload, headers={"Content-Type": "application/x-www-form-urlencoded"})
+    access_token = response.json()['access_token']
+    Spotify_PlaylistName, Spotify_PlaylistUserName = Spotify_PlaylistInfo(Spotify_PlaylistId, access_token)
+    if access_token:
+      with open('keys.json','w') as f:
+        data = {'spotfyAccessToken': access_token, 'Telegram_BotKey': Telegram_BotKey, 'Telegram_ChatId': Telegram_ChatId, 'Spotify_PlaylistUrl': Spotify_PlaylistUrl, 'Spotify_PlaylistId': Spotify_PlaylistId, 'Spotify_PlaylistName': Spotify_PlaylistName, 'Spotify_PlaylistUserName': Spotify_PlaylistUserName}
+        json.dump(data, f, indent=4)
+      break
+    time.sleep(5)
 
 
